fdd5241034e684e13d342c5e325395961503a7e9
thanack <thanaphat2543@gmail.com>
2021-01-15 17:13:30 +0700

    Lab11_4

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-
 using namespace std;
+
+template <typename currentType>
+void mySwap(currentType& ,currentType& );
 
 int main(){
 	int x, y;
@@ -28,5 +30,14 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename currentType>
+void mySwap(currentType &x , currentType &y)
+{
+	currentType temp;
+	temp = x;
+	x = y;
+	y = temp;
 }
 
